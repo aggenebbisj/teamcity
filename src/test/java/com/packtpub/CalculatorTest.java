d4b3897036b9b5a228e8231f8fb04bf961dfb9fe
Remko de Jong <aggenebbisj@gmail.com>
2013-06-18 21:23:58 +0200

    Added line to test

--- src/test/java/com/packtpub/CalculatorTest.java
+++ src/test/java/com/packtpub/CalculatorTest.java
@@ -9,5 +9,7 @@ public class CalculatorTest extends TestCase {
         Calculator calculator = new Calculator();
         int sum = calculator.sum(1, 2);
         Assert.assertEquals(3, sum);
+        Assert.assertEquals(4, sum + 1);
     }
 }
+
